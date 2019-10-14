5f400530539318ee223dda227880ff1d61d089f4
vladimirtischler <tischler.vladimir@gmail.com>
2019-10-14 10:02:26 +0200

    open lab 02.4

---
+++ open-lab-02.4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace open_lab_02._4
+{
+    class Program
+    {
+        public static int GetLegsCount(int chickens, int cows, int pigs)
+        {
+            return (chickens + cows + pigs);
+        }
+        static void Main(string[] args)
+        {
+            int chickenlegs = 2;
+            int cowslegs = 4;
+            int pigslegs = 4;
+            Console.WriteLine(GetLegsCount(chickenlegs, cowslegs, pigslegs));
+        }
+    }
+}
