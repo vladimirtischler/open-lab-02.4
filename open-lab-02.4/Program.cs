408aa3bd0837692bfa28fcfffef098b5f79609bd
vladimirtischler <56106321+vladimirtischler@users.noreply.github.com>
2019-10-16 21:41:32 +0200

    Changes

--- open-lab-02.4/Program.cs
+++ open-lab-02.4/Program.cs
@@ -1,19 +1,23 @@
-﻿using System;
+using System;
 
-namespace open_lab_02._4
+namespace ConsoleApp2
 {
+    
     class Program
     {
-        public static int GetLegsCount(int chickens, int cows, int pigs)
+        public static int GetLegsCount(int cows, int chicken, int pigs)
         {
-            return (chickens + cows + pigs);
+            return (cows * 4 + chicken * 2 + pigs * 4);
         }
         static void Main(string[] args)
         {
-            int chickenlegs = 2;
-            int cowslegs = 4;
-            int pigslegs = 4;
-            Console.WriteLine(GetLegsCount(chickenlegs, cowslegs, pigslegs));
+            Console.WriteLine("Enter the count of cows!");
+            int cows = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the count of chickens!");
+            int chickens = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the count of pigs!");
+            int pigs = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"The count of legs on the farm is {(GetLegsCount(cows, chickens,pigs))}.");
         }
     }
 }
